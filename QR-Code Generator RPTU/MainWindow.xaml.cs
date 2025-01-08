f97ded817274112d526e13dd58276424aea508a2
Infernoby99 <damianiturri.di@gmail.com>
2025-01-08 11:15:19 +0100

    test

--- QR-Code Generator RPTU/MainWindow.xaml.cs
+++ QR-Code Generator RPTU/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using QR_Code_Generator_RPTU.Data;
+using QR_Code_Generator_RPTU.Pages;
 using QR_Code_Generator_RPTU.renderer;
 
 namespace QR_Code_Generator_RPTU;
@@ -13,43 +14,8 @@ public partial class MainWindow
     public MainWindow()
     {
         InitializeComponent();
+        MainFrame.Navigate(new HomePage());
     }
 
-    private void Generate_OnClick(object sender, RoutedEventArgs e)
-    {
-        int level = ComboBox_CorrectionLevel.SelectedIndex;
-        int version = ComboBox_Version.SelectedIndex;
-        int mask = ComboBox_Mask.SelectedIndex;
-        QrCodeGenerator generate; 
-        if(level == 4 && version == 6) generate = new QrCodeGenerator(Canvas, Message);
-        else generate = new QrCodeGenerator(Canvas, Message, version, level);
-        if(generate.ErrorCheck())
-        {
-           
-            MessageBox.Show($"Text daten passen nicht! Wähle entweder eine höhere Version oder " +
-                            $"niedrigeren Korrekturlevel.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-        }
-        else
-        {
-            generate.QrCodeField();
-            generate.FindersPattern();
-            generate.AlignmentPattern();
-            generate.TimingPattern();
-            generate.MessageBits();
-            generate.MaskPattern(mask);
-        }
-    }
-
-    private void Message_OnTextChanged(object sender, TextChangedEventArgs e)
-    {
-        if (sender is TextBox tbox)
-        {
-            string content = tbox.Text;
-            if (content.Length > 0)
-            {
-                Generator.IsEnabled = true;
-            }
-            else Generator.IsEnabled = false;
-        }
-    }
+   
 }
---
+++ QR-Code Generator RPTU/Pages/FindersPattern.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace QR_Code_Generator_RPTU.Pages;
+
+public partial class FindersPattern : Page
+{
+    public FindersPattern()
+    {
+        InitializeComponent();
+    }
+}
